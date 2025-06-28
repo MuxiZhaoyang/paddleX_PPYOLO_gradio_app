79c0e01ad610d872d23ad57d0711053fc8ca06b1
Muxi <muxizhaoyang2024@163.com>
2025-06-29 02:16:49 +0800

    修改

--- Gradio.app.py
+++ Gradio.app.py
@@ -80,7 +80,7 @@ try:
                 #"model_dir": r"C:\ProgramCodeFiles\DL\CURSOR\gradio_paddle\inference",
                 #"model_dir": r"gradio_paddle\inference",
                 #"model_dir": r"\gradio_paddle\inference",
-                "model_dir": r"/home/aistudio/Gradio.app.py",
+                "model_dir": r"\home\aistudio\inference",
                 "batch_size": 1,
                 "threshold": 0.5
             }
